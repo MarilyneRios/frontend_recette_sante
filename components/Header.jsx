b0cd38399e818349d8cda3da1f33e5c7b1f54a4d
Rios marilyne <rios.marilyne@gmail.com>
2024-03-14 10:19:28 +0100

    mise en place de la structure + header

---
+++ components/Header.jsx
@@ -0,0 +1,70 @@
+import { Navbar, Nav, Container, NavDropdown } from "react-bootstrap";
+import { FaSignInAlt, FaSignOutAlt } from "react-icons/fa";
+import { CgProfile } from "react-icons/cg";
+import { TiUserAddOutline } from "react-icons/ti";
+
+const Header = () => {
+  return (
+    <header>
+      <Navbar bg="success" variant="dark" expand="lg" collapseOnSelect>
+        <Container>
+          <Navbar.Brand>Mon cahier de recettes</Navbar.Brand>
+
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="ms-auto">
+              <>
+                <NavDropdown title="userInfo.username" id="username">
+                  <NavDropdown.Item>
+                    <CgProfile /> Profile
+                  </NavDropdown.Item>
+                  <NavDropdown.Item>
+                    <FaSignOutAlt />
+                    Logout
+                  </NavDropdown.Item>
+                </NavDropdown>
+                <NavDropdown title="Filtrer par catégories" id="category">
+                <NavDropdown.Item>
+                     Apéro
+                  </NavDropdown.Item>
+                  <NavDropdown.Item>
+                     Entrée
+                  </NavDropdown.Item>
+                  <NavDropdown.Item>
+                   Plat
+                  </NavDropdown.Item>
+                  <NavDropdown.Item>
+                   Dessert
+                  </NavDropdown.Item>
+                  <NavDropdown.Item>
+                    Boissons
+                  </NavDropdown.Item>
+
+                </NavDropdown>
+                <NavDropdown.Item className="text-white">
+                  Ajouter une recette
+                </NavDropdown.Item>
+                
+                <NavDropdown.Item className="text-white">
+                Mes recettes favorites
+                </NavDropdown.Item>
+              </>
+
+              <>
+                <Nav.Link>
+                  <FaSignInAlt /> Connexion
+                </Nav.Link>
+
+                <Nav.Link>
+                  <TiUserAddOutline /> Inscription
+                </Nav.Link>
+              </>
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+    </header>
+  );
+};
+
+export default Header;
---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import Header from "../components/Header"
+import './App.css'
+
+function App() {
+
+
+  return (
+    <>
+    <Header/>
+  <h1>Mon cahier de recettes</h1>
+ 
+    </>
+  )
+}
+
+export default App
