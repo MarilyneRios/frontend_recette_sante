415b2504f4e9f03107609997aa790b979d491a84
Rios marilyne <rios.marilyne@gmail.com>
2024-03-20 08:45:01 +0100

    ajustement de la grid readRecipeS

--- src/screens/readRecipeS.jsx
+++ src/screens/readRecipeS.jsx
@@ -1,12 +1,13 @@
+/* eslint-disable react/jsx-key */
 import React, { useState } from "react";
 import { useSelector } from "react-redux";
-import { Card, Button} from "react-bootstrap";
+import { Card, Button, Container, Row, Col } from "react-bootstrap";
 import bookImage from "../assets/book.png";
 import { useAllRecipesAuthQuery } from "../slices/recipesApiSlice";
 import { FaHeart } from "react-icons/fa";
 import Loader from "../components/Loader";
 
-function ReadRecipeS({recipes, currentPage}) { 
+function ReadRecipeS({ recipes, currentPage }) {
   const [liked, setLiked] = useState(false);
 
   const { userInfo } = useSelector((state) => state.auth);
@@ -26,43 +27,58 @@ function ReadRecipeS({recipes, currentPage}) {
   const currentRecipes = recipes.slice(indexOfFirstRecipe, indexOfLastRecipe);
 
   return (
-    <> 
+    <>
       {isLoading && <Loader />}
       {isError && <h3>Something is wrong ...</h3>}
-     
       {isSuccess &&
-        currentRecipes.map((recipe) => (
-          // eslint-disable-next-line react/jsx-key
-          <div className="m-5">
-          <div className="col-sm-4 p-2">
-          <Card 
-          key={recipe.id} 
-          className="m-2 "
-          style={{ width: "16rem", position: "relative" }}>
-            <Button
-              variant="outline-primary"
-              onClick={toggleLike}
-              className="mx-1 mt-1 border   border-white rounded-circle"
-              style={{ position: "absolute", top: 0, right: 0 }}
-            >
-              {liked ? (
-                <FaHeart size={30} color="red" />
-              ) : (
-                <FaHeart size={30} color="gray" />
-              )}
-            </Button>
-            <Card.Img variant="top" src={recipe.imageUrl || bookImage} className="mx-5 mt-2 w-50" />
-            <Card.Body>
-              <Card.Title className="text-center"> {recipe.name}</Card.Title>
-              <Card.Text  className="text-center"> {recipe.category}</Card.Text>
-              <Card.Text  className="text-center">De {recipe.pseudo}</Card.Text>
-            </Card.Body>
-          </Card>
-          </div>
-          </div>
-        ))}
+        <Container className="w-75">
+          <Row>
+            {currentRecipes.map((recipe) => (
+              <Col md={4}>
+                <Card
+                  key={recipe.id}
+                  className="m-3 "
+                  style={{ width: "16rem", position: "relative" }}
+                >
+                  <Button
+                    variant="outline-primary"
+                    onClick={toggleLike}
+                    className="mx-1 mt-1 border   border-white rounded-circle"
+                    style={{ position: "absolute", top: 0, right: 0 }}
+                  >
+                    {liked ? (
+                      <FaHeart size={30} color="red" />
+                    ) : (
+                      <FaHeart size={30} color="gray" />
+                    )}
+                  </Button>
+                  <Card.Img
+                    variant="top"
+                    src={recipe.imageUrl || bookImage}
+                    className="mx-5 mt-2 w-50"
+                  />
+                  <Card.Body>
+                    <Card.Title className="text-center">
+                      {" "}
+                      {recipe.name}
+                    </Card.Title>
+                    <Card.Text className="text-center">
+                      {" "}
+                      {recipe.category}
+                    </Card.Text>
+                    <Card.Text className="text-center">
+                      De {recipe.pseudo}
+                    </Card.Text>
+                  </Card.Body>
+                </Card>
+              </Col>
+            ))}
+          </Row>
+        </Container>
+      }
     </>
   );
+
 }
 
 export default ReadRecipeS;
