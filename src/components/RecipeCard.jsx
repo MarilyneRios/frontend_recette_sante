1899ba78398c5aaf2e58705983d56aa343e89958
Rios marilyne <rios.marilyne@gmail.com>
2024-04-19 17:10:46 +0200

    add et remove recipe dans favorite logique

--- src/components/RecipeCard.jsx
+++ src/components/RecipeCard.jsx
@@ -1,30 +1,85 @@
-import { useState } from "react";
+import { useState,useEffect } from "react";
 import { useSelector } from "react-redux";
-import { Card, Button, ListGroup, Col } from "react-bootstrap";
+import { Card, Button, Col } from "react-bootstrap";
 import bookImage from "../assets/book.png";
 import { FaHeart, FaRegHeart } from "react-icons/fa";
-import { useNavigate } from 'react-router-dom';
+import { useNavigate } from "react-router-dom";
+import {
+  useAddRecipeFavoriteMutation,
+  useRemoveRecipeFavoriteMutation,
+  useAllRecipesFavoriteQuery,
+
+} from "../slices/recipesApiSlice";
 
 // eslint-disable-next-line react/prop-types
 function RecipeCard({ recipe }) {
-  const [liked, setLiked] = useState(false);
-
 
   const { userInfo } = useSelector((state) => state.auth);
+  const [liked, setLiked] = useState(false);
+  const {data: favoriteRecipes} = useAllRecipesFavoriteQuery()
+  const [addRecipeFavorite] = useAddRecipeFavoriteMutation();
+  const [removeRecipeFavorite] = useRemoveRecipeFavoriteMutation();
 
   const navigate = useNavigate();
 
-  const handleReadRecipe = async (e) => {
+  //Favoris
+  useEffect(() => {
+    console.log("favoriteRecipes useEffect",favoriteRecipes);
+    console.log("Checking if recipe is in favoriteRecipes"); 
+    if (favoriteRecipes && favoriteRecipes.some(favRecipe => favRecipe._id === recipe._id)) {
+      console.log("Recipe is in favoriteRecipes");
+      setLiked(true);
+    } else {
+      console.log("Recipe is not in favoriteRecipes");
+      setLiked(false);
+    }
+  }, [favoriteRecipes, recipe._id]);
+  
+
+  const handleReadRecipe = (e) => {
     e.preventDefault();
-   navigate(`/viewRecipeAuth/${recipe._id}`);
+    navigate(`/viewRecipeAuth/${recipe._id}`);
   };
 
-  const toggleLike = async (e) => {
+  const toggleLike = (e) => {
     e.preventDefault();
     setLiked(!liked);
-   // ajouter une recette dans les favoris avec cette route`/addRecipeFavorite/${recipe._id}`;
+    if (!liked) {
+      handleAddRecipeFavorite();
+    } else {
+      handleremoveRecipeFavorite();
+    }
   };
 
+  const handleAddRecipeFavorite = () => {
+    
+    if(favoriteRecipes && favoriteRecipes._id === recipe._id){
+      console.log("Recipe is already in Favorite")
+    }
+    else{
+      setLiked(!liked);
+      addRecipeFavorite(recipe._id)
+      .then((response) => {
+        console.log("RecipeFavorite add:", response);
+      })
+      .catch((error) => {
+        console.error("Error add RecipeFavorite:", error);
+      });
+    }
+
+  };
+
+  const handleremoveRecipeFavorite = () => {
+    setLiked(!liked);
+
+    removeRecipeFavorite(recipe._id)
+      .then((response) => {
+        console.log("RecipeFavorite remove:", response);
+      })
+      .catch((error) => {
+        console.error("Error remove RecipeFavorite:", error);
+      });
+  };
 
   return (
     <Col md={4}>
@@ -35,9 +90,8 @@ function RecipeCard({ recipe }) {
         style={{
           width: "18rem",
           height: "30rem",
-          margin: "20px auto", 
+          margin: "20px auto",
         }}
-   
       >
         <Button
           variant="outline-primary"
@@ -49,6 +103,7 @@ function RecipeCard({ recipe }) {
             <FaHeart size={30} color="red" />
           ) : (
             <FaRegHeart size={30} color="black" />
+
           )}
         </Button>
         <Card.Img
@@ -60,28 +115,23 @@ function RecipeCard({ recipe }) {
         />
         <Card.Body>
           <Card.Title className="text-center fs-4">{recipe.name}</Card.Title>
-          <Card.Text className="text-center fs-5">
-            {recipe.category}
-          </Card.Text>
-        
+          <Card.Text className="text-center fs-5">{recipe.category}</Card.Text>
+
           <Card.Text className="text-center">
             Les bienfaits : {recipe.comments}
           </Card.Text>
           <Card.Text className="text-center ">
-            <em>Auteur : {recipe.pseudo || 'inconnu'}</em>
+            <em>Auteur : {recipe.pseudo || "inconnu"}</em>
           </Card.Text>
 
           <div className="d-flex justify-content-center">
-          
-              <Button
-                onClick={handleReadRecipe}
-                className=" btn btn-lg btn-dark rounded mx-5 my-3"
-              >
-                Lire
-              </Button>
-
-        </div>
-
+            <Button
+              onClick={handleReadRecipe}
+              className=" btn btn-lg btn-dark rounded mx-5 my-3"
+            >
+              Lire
+            </Button>
+          </div>
         </Card.Body>
       </Card>
     </Col>
