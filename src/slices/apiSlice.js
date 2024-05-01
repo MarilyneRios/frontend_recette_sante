a7a0921df5ffe3fbfea3f8d1744ff65cda176a55
Rios marilyne <rios.marilyne@gmail.com>
2024-05-01 15:17:34 +0200

    add render server

--- src/slices/apiSlice.js
+++ src/slices/apiSlice.js
@@ -2,7 +2,7 @@
 import { fetchBaseQuery, createApi } from '@reduxjs/toolkit/query/react';
 
 //configuration d'une requête de base pour les appels d'API
-const baseQuery = fetchBaseQuery({ baseUrl: 'https://backend-recette-sante.vercel.app' });
+const baseQuery = fetchBaseQuery({ baseUrl: 'https://backend-recette-sante.onrender.com' });
 //const baseQuery = fetchBaseQuery({ baseUrl: '' });
 //création d'API qui est un objet de configuration avec plusieurs propriétés.
 export const apiSlice = createApi({
